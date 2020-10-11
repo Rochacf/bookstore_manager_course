7006613754ecfa8d7f4f09191a5b38b3a0c2b2ae
Cristiano Ferreira <cristianofrocha@gmail.com>
2020-10-10 22:16:53 -0300

    Developed project entities

---
+++ src/main/java/com/rodrigopeleias/bookstoremanager/entity/Author.java
@@ -0,0 +1,25 @@
+package com.rodrigopeleias.bookstoremanager.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data     /* Automaticamente passamos a ter GET e SET dos atributos */
+@Builder  /* Fornecerá uma classe Builder para criação de componentes seguros */
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+}
