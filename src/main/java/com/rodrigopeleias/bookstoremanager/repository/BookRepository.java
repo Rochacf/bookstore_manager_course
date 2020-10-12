0ba544bce385dd066fc4e0458eb376797a1fcdf8
Cristiano Ferreira <cristianofrocha@gmail.com>
2020-10-12 00:23:38 -0300

    Refactores functionality for book creation

--- src/main/java/com/rodrigopeleias/bookstoremanager/repository/BookRepository.java
+++ src/main/java/com/rodrigopeleias/bookstoremanager/repository/BookRepository.java
@@ -3,5 +3,5 @@ package com.rodrigopeleias.bookstoremanager.repository;
 import com.rodrigopeleias.bookstoremanager.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepgit ository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long> {
 }
